edaac506b7042f0af1693d9ce5bc00ba34d50a92
TsXor <zhang050525@qq.com>
2023-06-30 12:24:20 +0800

    fix vla workaround __STDC_NO_VLA__ should be defined in msvc, but for some mysterious reasons it is not defined, so define it manually.

--- workarounds/VLA.h
+++ workarounds/VLA.h
@@ -1,4 +1,8 @@
-#if defined(__STDC_NO_VLA__)
+#ifdef _MSC_VER
+#    define __STDC_NO_VLA__ 1
+#endif
+
+#ifdef __STDC_NO_VLA__
 #    ifdef _MSC_VER
 #        include <malloc.h>
 #        define alloca _alloca
