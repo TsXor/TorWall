1f99214335b8461c26a2a91673d616772eb489ab
TsXor <zhang050525@qq.com>
2023-06-30 10:22:03 +0800

    change VLA workaround

--- workarounds/VLA.h
+++ workarounds/VLA.h
@@ -1,9 +1,9 @@
-#if defined(__GNUC__)
-#    define VLA_DECL(name, type, size) type name[size]
-#else
+#if defined(__STDC_NO_VLA__)
 #    ifdef _MSC_VER
 #        include <malloc.h>
 #        define alloca _alloca
 #    endif
 #    define VLA_DECL(name, type, size) type* name = (type* )alloca(size)
+#else
+#    define VLA_DECL(name, type, size) type name[size]
 #endif
